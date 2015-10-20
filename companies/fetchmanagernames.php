596c3d4ef4e6fdd042e0bed2f8480fb02d2805c0
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2015-10-20 12:47:20 +0200

    removed deleted files

--- companies/fetchmanagernames.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:dbname=practice_companies;host=localhost', 'root', null, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-$result = $pdo->query('SELECT DISTINCT manager_name FROM companies_detailed');
-$rows = $result->fetchAll(PDO::FETCH_ASSOC);
-echo(json_encode($rows));
-?>
--- mailing/fetchmailingaddresses.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO('mysql:dbname=practice_companies;host=localhost', 'root', null, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-$query = $pdo->query('SELECT sender_name, sender_address FROM mail_addresses');
-$result = $query->fetchAll(PDO::FETCH_ASSOC);
-echo(json_encode($result));
-?>
